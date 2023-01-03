65396a0396301e352981fed3b56903933af9d8b8
Tulio <tulio.mminini@gmail.com>
2023-01-03 18:35:50 -0300

    :lipstick: HomeTop section started

--- art-gallery-website/src/components/Home/HomeTopSection/HomeTop.jsx
+++ art-gallery-website/src/components/Home/HomeTopSection/HomeTop.jsx
@@ -1,5 +1,26 @@
 import style from "./HomeTop.module.css";
+import { NavLink } from "react-router-dom";
+import arrowRight from "./../../../assets/icon-arrow-right.svg";
 
 export function HomeTop() {
-  return <div className={style.homeTopContainer}>Home Top</div>;
+  return (
+    <section className={style.homeTopContainer}>
+      <h1>
+        Modern <br /> Art Gallery
+      </h1>
+      <p>
+        The arts in the collection of the Modern Art Gallery all started from a
+        spark of inspiration. Will these pieces inspire you? Visit us and find
+        out.
+      </p>
+      <div>
+        <NavLink to="/location" style={{ textDecoration: "none" }} className={style.location}>
+          <button className={style.btn1}>Our Location</button>
+          <button className={style.btn2}>
+            <img src={arrowRight} alt="arrow right icon" />
+          </button>
+        </NavLink>
+      </div>
+    </section>
+  );
 }
