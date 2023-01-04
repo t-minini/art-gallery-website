022cc805b5ca97185a62e5202223370b19d7cfd0
Tulio <tulio.mminini@gmail.com>
2023-01-04 20:36:01 -0300

    :lipstick: footer comp 100perc done

--- art-gallery-website/src/components/FooterSection/Footer.jsx
+++ art-gallery-website/src/components/FooterSection/Footer.jsx
@@ -1,5 +1,35 @@
 import style from "./Footer.module.css";
+import facebook from "./../../assets/icon-facebook.svg";
+import instagram from "./../../assets/icon-instagram.svg";
+import twitter from "./../../assets/icon-twitter.svg";
 
 export function Footer() {
-  return <div className={style.footerContainer}>Footer</div>;
+  return (
+    <footer className={style.footerContainer}>
+      <h3>
+        Modern <br /> Art Gallery
+      </h3>
+      <p>
+        The Modern Art Gallery is free to all visitors and open seven days a
+        week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+      </p>
+      <div className={style.icons}>
+        <img
+          src={facebook}
+          alt="facebook icon in white color"
+          className={style.facebook}
+        />
+        <img
+          src={instagram}
+          alt="instagram icon in white color"
+          className={style.instagram}
+        />
+        <img
+          src={twitter}
+          alt="twitter icon in white color"
+          className={style.twitter}
+        />
+      </div>
+    </footer>
+  );
 }
