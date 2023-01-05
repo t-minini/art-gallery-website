fd1e29aa944e985bb8df89f234d0454ecb8e0e4d
Tulio <tulio.mminini@gmail.com>
2023-01-04 21:10:44 -0300

    :lipstick: locationTop comp done

--- art-gallery-website/src/components/Location/LocationTopSection/LocationTop.jsx
+++ art-gallery-website/src/components/Location/LocationTopSection/LocationTop.jsx
@@ -1,5 +1,26 @@
+import { NavLink } from "react-router-dom";
 import style from "./LocationTop.module.css";
+import map from "./../../../assets/desktop/image-map@2x.png";
+import arrowLeft from "./../../../assets/icon-arrow-left.svg";
 
 export function LocationTop() {
-  return <div className={style.locationTopContainer}>Location</div>;
+  return (
+    <section className={style.locationTopContainer}>
+      <img
+        src={map}
+        alt="map location for the Modern Art Gallery"
+        className={style.map}
+      />
+      <NavLink
+        to="/"
+        style={{ textDecoration: "none" }}
+        className={style.locationBtn}
+      >
+        <button className={style.btn2}>
+          <img src={arrowLeft} alt="arrow left icon" />
+        </button>
+        <button className={style.btn1}>back to home</button>
+      </NavLink>
+    </section>
+  );
 }
